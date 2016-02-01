4e5808658a27804e37c1f329300986f2e574bc0d
GlovesforATT <Jeffrey@Windows.local>
2016-01-31 19:44:28 -0800

    FINISHED POMODORO

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -1,56 +1,92 @@
 var startButton = document.getElementById("start"); // gives different variable names to html button id's
 var stopButton = document.getElementById("stop");
-var resetButton = document.getElementById("reset");
 var minsLeft = document.getElementById("mins-left"); // this is a span id. <span> tag is used to group inline-elements in a document.
 var secsLeft = document.getElementById("secs-left"); // this is a span id
+var countJS = document.getElementById('count');
+var resetButton = document.getElementById('reset');
+
+
+	
 
 var startTimer = function(){
 	console.log("Start");
-  begin = setInterval(countdown, 50); // Call countdown function every 1000 milliseconds and defines begin
+  begin = setInterval(countdown, 1000); // Call countdown function every 1000 milliseconds and defines begin
 	startButton.disabled=true; // disables element in HTML. grays it out.
 	stopButton.disabled=false; // the order of these don't matter.
+	resetButton.disabled=true;
+
 }
 
 var stopTimer = function(){ // 
 	console.log("Stop");
-	window.clearInterval(begin) // clear the timer which stops the watch
+	window.clearInterval(begin) // stops the watch, it won't execute the commands below this line if it is at state 'begin'. (start hasn't been push yet)
 	stopButton.disabled=true;  // stop button is disabled when pressed
 	startButton.disabled=false; // start button becomes clickable
+	resetButton.disabled=false;
 }
 
 var resetTimer = function(){
 	console.log("Reset");
-	window.clearInterval(begin) // clear the timer and so stop the clock
-	secsLeft.innerText=00; // gets the secs-Left in HTML, which was assigned to secLeft in JS, and sets it to 00.
-	minsLeft.innerText=document.getElementById("mins").innerText; // when reseting, it takes the initial length of time set for the timer and resets it to that number.
-	stopButton.disabled=true; // disables stop button
-	startButton.disabled=false; // enables start button
+	minsLeft.innerHTML = 25;
+	secsLeft.innerHTML = 00;
+	startButton.disabled = false;
+	stopButton.disabled = true;
+
 }
+
 
 // This is what makes the clock tick downwards
 var countdown = function(){ // calls function countdown, which begins when pressing the start button. 
 	console.log("countdown"); 
-	if(secsLeft.innerText<=0){ // so basically, at 0, it resets to 60, then counts down.
-		secsLeft.innerText=60;
-		minsLeft.innerText--; // minute goes down by one when secs = 60		
+	if(secsLeft.innerHTML<=0){ // so basically, at 0, it resets to 60, then counts down.
+		secsLeft.innerHTML=60;
+		minsLeft.innerHTML--; // minute goes down by one when secs = 60		
 	};
-	secsLeft.innerText--; // keeps the seconds going down by one.  
+	secsLeft.innerHTML--; // keeps the seconds going down by one.  
 	
-	if(secsLeft.innerText<=0 && minsLeft.innerText<=0){ // when mins and sec == 0, it will play the audio
-		window.clearInterval(begin); 
+	if(secsLeft.innerHTML<=0 && minsLeft.innerHTML<=0){ // when mins and sec == 0, it will play the audio
+		stopButton.disabled=true;
+		window.clearInterval(begin);  // stops the watch at 0, still allows below to execute.
 		var audio = new Audio('wc3-im-awake.wav');
-		audio.load();
 		audio.play(); // audio doesn't work on mobile
+		countJS.innerHTML++;
+		resetButton.disabled = false;
 	};
 }
 
-var more = function(){
-	document.getElementById("mins").innerText++;
-	minsLeft.innerText = document.getElementById("mins").innerText;
+
+
+
+
+
+function change() {
+	jsMinsTextfield = document.getElementById('mins-textfield').value; // can't use hypens for js variables.
+	jsMinsTextfield = Math.floor(jsMinsTextfield);
+	jsSecsTextfield = document.getElementById('secs-textfield').value;
+	jsSecsTextfield = Math.floor(jsSecsTextfield);
+	
+
+	 if( jsMinsTextfield<0 || jsSecsTextfield<0){
+       alert('Please use positive numbers.');
+       return;}
+     if (jsSecsTextfield>=60){
+     	alert('Acceptable values for seconds are from : 0 - 59');
+    return;
+   }
+   if (jsMinsTextfield == 0 && jsSecsTextfield == 0){
+   	alert('Please set a higher value.');
+   	return;
+   }
+   stopTimer();
+
+
+	jsMinsLeft = document.getElementById('mins-left');
+	jsSecsLeft = document.getElementById('secs-left');
+	jsMinsLeft.innerHTML = jsMinsTextfield;
+	jsSecsLeft.innerHTML = jsSecsTextfield;
+	startButton.disabled = false;
+
+	
 }
 
-var less = function(){
-	document.getElementById("mins").innerText--;
-	if(document.getElementById("mins").innerText<=1){document.getElementById("mins").innerText=1};
-	minsLeft.innerText = document.getElementById("mins").innerText;
-}
+

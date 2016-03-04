b94b7728254b48e4c421f3d7f2ca4c1c322be45c
GlovesforATT <dinhjeffreyy@gmail.com>
2016-03-04 08:59:19 -0800

    updated getLocation() to https because in the future it will be deprecated

--- weather/weather.js
+++ weather/weather.js
@@ -46,7 +46,7 @@ function geoLocation() {
 // uses api.openweathermap.org for weather
 
 function weather(city){
-  var url = "http://api.openweathermap.org/data/2.5/weather?";
+  var url = "https://api.openweathermap.org/data/2.5/weather?";
   var key = "3a53d3f03d189f460e7bd9e53adfa628"
   //console.log(url + 'q=' + city + "&APPID=" + key +"&units=metric");
   // main is temp, pressure humidity, temp min, temp max
@@ -57,9 +57,9 @@ function weather(city){
   $.getJSON(url + 'q=' + city + "&APPID=" + key +"&units=metric", function(data){
     var main = data.main, weather = data.weather[0], temp = main.temp.toFixed(1), name = data.name, 
     country = data.sys.country, sunrise = new Date(data.sys.sunrise * 1000), sunset = new Date(data.sys.sunset * 1000); 
-    var sunIcon = "http://findicons.com/files/icons/2232/wireframe_mono/16/sun.png";
+    var sunIcon = "https://findicons.com/files/icons/2232/wireframe_mono/16/sun.png";
     document.getElementById('sunrise').innerHTML = '<img src='+sunIcon+'>' + sunrise.getHours() + ":" + sunrise.getMinutes()+"am"; 
-    var moonIcon = "http://findicons.com/files/icons/1667/iconic/12/moon_fill.png"; 
+    var moonIcon = "https://findicons.com/files/icons/1667/iconic/12/moon_fill.png"; 
     document.getElementById('sunset').innerHTML = '<img src='+moonIcon+'>' + Number(sunset.getHours()-12) + ":" + sunset.getMinutes()+"pm";
     document.getElementById('temp').innerHTML = (32 + temp * 1.8).toFixed(0) +  "&deg; F";      
     document.getElementById('city').innerHTML = name;
@@ -93,7 +93,7 @@ $('.link').click(function(){
 
 // use // instead of https:// to avoid "sorry no imagery here"
 function background(city){
-  var viewUrl = "//maps.googleapis.com/maps/api/streetview?size=400x400&location=" + city +"&fov=90&heading=235&pitch=10"; 
+  var viewUrl = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" + city +"&fov=90&heading=235&pitch=10"; 
   $('body').append("<style>html { background: url(" + viewUrl + ") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover;          -o-background-size: cover;          background-size: cover;        }      </style>");  
 }
 function setBackgroundGif(type){
@@ -105,7 +105,7 @@ function setBackgroundGif(type){
     haze: "https://chicgeekspeaks.files.wordpress.com/2014/04/tumblr_n2s1pkcyib1r93041o1_500.gif",
     mist: "https://chicgeekspeaks.files.wordpress.com/2014/04/tumblr_n2s1pkcyib1r93041o1_500.gif",
     dust: "https://s-media-cache-ak0.pinimg.com/originals/c8/db/48/c8db4861b237898512da1c1f35e87231.jpg",
-    snow: "http://33.media.tumblr.com/d7502507d4b5974314ae72e61ee581db/tumblr_nb32o9WTxa1r8zm42o1_500.gif"
+    snow: "https://33.media.tumblr.com/d7502507d4b5974314ae72e61ee581db/tumblr_nb32o9WTxa1r8zm42o1_500.gif"
   }  
   $('.main').append("<style>.main { background: url(" + gifs[type.toLowerCase()] + ") no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover;          background-size: cover;        }</style>");    
 }

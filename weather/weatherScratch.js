665d6f0cdf2d4c2b9245737b75e2a2e3bd104ac1
GlovesforATT <dinhjeffreyy@gmail.com>
2016-03-04 10:50:53 -0800

    updated weatherScratch

--- weather/weatherScratch.js
+++ weather/weatherScratch.js
@@ -1,30 +1,56 @@
 
+// function geoLocation() {
+//   console.log("1")
+//   function getCity(latlng) {
+//     console.log("4")
+//     var geocoder = new google.maps.Geocoder();
+//     geocoder.geocode({
+//       'latLng': latlng
+//     }, function(results, status) {
+//       if (status == google.maps.GeocoderStatus.OK) {
+//               //console.log(results[0].address_components);
+//               if (results[0]) {
+//                 var city;
+//                 var i = 0;
+//                 while (!city && i < results[0].address_components.length) {
+//                   if (results[0].address_components[i].types.indexOf("locality") >= 0) {
+//                     city = results[0].address_components[i].long_name;                                       
+//                     weather(city); // not defined yet
+//                   }
+//                   i++;
+//                 }
+//               }
+//             } 
+//             else {
+//               console.log("Could not find city");
+//             }
+//           });
+//   }
+
 function geoLocation() {
-  console.log("1")
+  console.log('1');
   function getCity(latlng) {
-    console.log("4")
+    console.log("4");
+    // https://developers.google.com/maps/documentation/javascript/geocoding#GeocodingStatusCodes
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({
       'latLng': latlng
     }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-              //console.log(results[0].address_components);
-              if (results[0]) {
-                var city;
-                var i = 0;
-                while (!city && i < results[0].address_components.length) {
-                  if (results[0].address_components[i].types.indexOf("locality") >= 0) {
-                    city = results[0].address_components[i].long_name;                                       
-                    weather(city);
-                  }
-                  i++;
-                }
-              }
-            } 
-            else {
-              console.log("Could not find city");
+        console.log(results[0].address_components);
+        if (results[0]) {
+          var city;
+          var i = 0;
+          while (!city && i< results[0].address_components.length) {
+            if (results[0].address_components[i].types.indexOf("locality") >= 0) {
+              city = results[0].address_components[i].long_name;
+              weather(city); // weather() function not defined yet
             }
-          });
+            i++;
+          }
+        }
+      } else { console.log("Could not find city"); }
+    });
   }
 
   function processUserLoc() {

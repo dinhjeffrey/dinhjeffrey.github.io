08c5a8055ac73f5db695aa824492e36fdc5679aa
GlovesforATT <dinhjeffreyy@gmail.com>
2016-03-04 10:13:52 -0800

    added weatherjs built from scratch

--- weather/weatherScratch.js
+++ weather/weatherScratch.js
@@ -1,3 +1,62 @@
-function processUserLoc() {
-   
+
+function geoLocation() {
+  console.log("1")
+  function getCity(latlng) {
+    console.log("4")
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+      'latLng': latlng
+    }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+              //console.log(results[0].address_components);
+              if (results[0]) {
+                var city;
+                var i = 0;
+                while (!city && i < results[0].address_components.length) {
+                  if (results[0].address_components[i].types.indexOf("locality") >= 0) {
+                    city = results[0].address_components[i].long_name;                                       
+                    weather(city);
+                  }
+                  i++;
+                }
+              }
+            } 
+            else {
+              console.log("Could not find city");
+            }
+          });
+  }
+
+  function processUserLoc() {
+    console.log("2")
+      // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
+      if ("geolocation" in navigator) {
+        console.log("3")
+        // if geolocation is available
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          getCity(latlng);
+        });
+      } else {
+        //error function - location not available
+        alert("location is not available")
+      }
+  }
+    
+
+  processUserLoc();
 }
+
+// uses api.openweathermap.org for weather
+
+
+
+
+
+
+
+$(document).ready(function(){
+  console.log("0")
+  geoLocation();
+// done
+});
